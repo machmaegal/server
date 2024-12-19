0a1bb1782e6a4d2d8cba4830a7152128f7262894
machmaegal <aka.pleat371@passinbox.com>
2024-12-19 10:12:57 +0100

    changes for vercel config

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require('dotenv').config()
+require('dotenv').config();
 
 // ℹ️ Connects to the database
-require('./db')
+require('./db');
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-require('./config')(app)
+require('./config')(app);
 
 // 👇 Start handling routes here
 // const indexRoutes = require('./routes/index.routes')
 // app.use('/users', indexRoutes)
-const authorization = require('./routes/auth.routes')
-app.use('/auth', authorization)
-const foodRoute = require('./routes/food.routes')
-app.use('/food', foodRoute)
-const drinkRoute = require('./routes/drink.routes')
-app.use('/drink', drinkRoute)
-const users = require('./routes/user.routes')
-app.use('/users', users)
-const orderRoute = require('./routes/order.routes')
-app.use('/orders', orderRoute)
+const authorization = require('./routes/auth.routes');
+app.use('/auth', authorization);
+const foodRoute = require('./routes/food.routes');
+app.use('/food', foodRoute);
+const drinkRoute = require('./routes/drink.routes');
+app.use('/drink', drinkRoute);
+const users = require('./routes/user.routes');
+app.use('/users', users);
+const orderRoute = require('./routes/order.routes');
+app.use('/orders', orderRoute);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require('./error-handling')(app)
+require('./error-handling')(app);
 
-module.exports = app
+module.exports = app;
